472408cde0c58b529184aa8c316e7e62a73275d5
jmmrcpsip <jmmrcpsip@gmail.com>
2016-03-08 09:07:09 +0100

    Angry Professor

--- Implementation/Angry Professor/main.go
+++ Implementation/Angry Professor/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var (
+		t uint // Numero de casos
+		k int // Requeridos
+		n uint // Numero de alumnos
+		a int  // llegadas
+		i uint
+		j uint
+	)
+
+	fmt.Scanf("%d", &t) // 2
+	for i = 0; i < t; i++ {
+		fmt.Scanf("%d", &n) // 4
+		fmt.Scanf("%d", &k) // 3
+		for j = 0; j < n; j++ {
+			fmt.Scanf("%d", &a) // -1 -3 4 2
+			if a <= 0 {
+				k--
+			}
+		}
+		if k > 0 {
+			fmt.Println("YES")
+		} else {
+			fmt.Println("NO")
+		}
+	}
 }

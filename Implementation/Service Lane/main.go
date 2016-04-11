378536878bb02b690a82a2f9b2ad7071ae2a2e09
Jose Martinez <jmmrcp@gmail.com>
2016-04-11 13:03:58 +0200

    Service Lane

--- Implementation/Service Lane/main.go
+++ Implementation/Service Lane/main.go
@@ -2,17 +2,46 @@ package main
 
 import "fmt"
 
+// Min ...
+func Min(x, y uint) uint {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func vehicle(x, y uint, s []uint) {
+	var min uint = 999
+	for i := x; i <= y; i++ {
+		min = Min(min, s[i])
+	}
+	fmt.Println(min)
+}
 func main() {
 	var (
-		t  uint   // Numero de casos
-		n1 uint32 // valor
-		n2 uint32 // valor
-		i  uint   // variable para el bucle
+		t uint // Numero de elementos del Array
+		n uint // Numero de segmentos a comparar
+		i uint // variable para el bucle
+		v uint // valor en el array
+
+		inicio uint
+		final  uint
 	)
 
 	fmt.Scanf("%d", &t)
+	fmt.Scanf("%d", &n)
+	slice := make([]uint, t)
+
 	for i = 0; i < t; i++ {
-		fmt.Scanf("%d", &n1)
-		fmt.Scanf("%d", &n2)
+		fmt.Scanf("%d", &v)
+		slice[i] = v
+	}
+	// fmt.Println("Valor del Slice", slice)
+
+	for i = 0; i < n; i++ {
+		fmt.Scanf("%d", &inicio)
+		fmt.Scanf("%d", &final)
+
+		vehicle(inicio, final, slice)
 	}
 }

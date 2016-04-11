7639ceec66e273e9fe9841f71ff75b9c90d7d937
Jose Martinez <jmmrcp@gmail.com>
2016-04-11 17:25:51 +0200

    Tarde

---
+++ Implementation/Cut the sticks/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func Min(x, y uint) uint {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+func Cut(s []uint) uint {
+	var min uint = 999
+	for _, n := range s {
+		min = Min(min, n)
+	}
+	return min
+}
+
+func Sticks(s []uint) []uint {
+	var a []uint
+	if len(s) > 0 {
+		n := Cut(s)
+		for _, v := range s {
+			d := v - n
+			if d > 0 {
+				a = append(a, d)
+			}
+		}
+		//fmt.Printf("l: %d, slice: %d", len(s), a)
+		fmt.Println(len(s))
+		return Sticks(a)
+	}
+	return a
+}
+
+func main() {
+
+	var N uint // numero de sticks
+	var l uint // longitud del stick
+	var i uint
+
+	fmt.Scanf("%d", &N)
+	a := make([]uint, N)
+
+	for i = 0; i < N; i++ {
+		fmt.Scanf("%d", &l)
+		a[i] = l
+	}
+	//fmt.Println(a)
+	//fmt.Println(Cut(a))
+	//fmt.Println(Sticks(a))
+	Sticks(a)
+}

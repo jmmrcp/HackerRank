3a6f882848e99af0e6bf077e2ba6facff50ae5bb
Jose Martinez <jmmrcp@gmail.com>
2016-04-12 11:36:52 +0200

    Project

--- Contests/ProjectEuler+/#1 Multiples of 3 and 5/main.go
+++ Contests/ProjectEuler+/#1 Multiples of 3 and 5/main.go
@@ -1,29 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func Multiples(N uint) {
-	var suma uint = 0
-    var i uint
-	for i = 0; i < N; i++ {
-		if i%3 == 0 || i%5 == 0 {
-			suma += i
-		} // End if
-	} // End for
-	fmt.Println(suma)
-    
+	N = (N - 1)
+	n := float64(N)
+	var (
+		n1   float64
+		n2   float64
+		n3   float64
+		suma float64
+	)
+	n1 = 3 * (math.Floor(n/3) * (math.Floor(n/3) + 1) / 2)
+	n2 = 5 * (math.Floor(n/5) * (math.Floor(n/5) + 1) / 2)
+	n3 = 15 * (math.Floor(n/15) * (math.Floor(n/15) + 1) / 2)
+
+	suma = n1 + n2 - n3
+	fmt.Println(uint64(suma))
 }
 
 func main() {
-    var (
-        T uint
-        N uint
-        i uint
-    )
-    fmt.Scanf("%d", &T)
-    for i = 0; i < T; i++ {
-        fmt.Scanf("%d", &N)
-        Multiples(N)
-    }
-	
+	var (
+		T uint
+		N uint
+		i uint
+	)
+	fmt.Scanf("%d", &T)
+	for i = 0; i < T; i++ {
+		fmt.Scanf("%d", &N)
+		Multiples(N)
+	}
+
 }
